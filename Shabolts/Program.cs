5815f4dea9d75b711744e38f093815d43438ec13
Andrew <asagura85@gmail.com>
2020-04-14 17:29:44 +0300

    Laba_6

--- Shabolts/Program.cs
+++ Shabolts/Program.cs
@@ -1,46 +1,121 @@
 ﻿using System;
-using System.IO;
-
-namespace Shabolts
+namespace ConsoleApp6
 {
+    // визначення інтерфейсу
+    interface IDemo
+    {
+        void Show(); //оголошення методу
+        double Dlina(); //оголошення методу
+        int X { get; } //оголошення властивості, доступного тільки для читання
+        int this[int i] { get; set; } //оголошення індексатора, доступного для
+        //читання-запису
+    }
+    // клас DemoPoint успадковує інтерфейс IDemo
+    class DemoPoint : IDemo
+    {
+        protected int x;
+        protected int y;
+        public DemoPoint(int x, int y)
+        {
+            this.x = x; this.y = y;
+        }
+        public void Show() //реалізація методу, оголошеного в інтерфейсі
+        {
+            Console.WriteLine(" точка на площині: ({ 0}, { 1})", x, y);
+        }
+        public double Dlina() //реалізація методу, оголошеного в інтерфейсі
+        {
+            return Math.Sqrt(x * x + y * y);
+        }
+        public int X //реалізація властивості, оголошеної в інтерфейсі
+        {
+            get
+            {
+                return x;
+            }
+        }
+        public int this[int i] //реалізація індексатора, оголошеного в інтерфейсі
+        {
+            get
+            {
+                if (i == 0) return x;
+
+               
+else if (i == 1) return y;
+                else throw new Exception(" неприпустиме значення індексу ");
+            }
+            set
+            {
+                if (i == 0) x = value;
+                else if (i == 1) y = value;
+                else throw new Exception(" неприпустиме значення індексу ");
+            }
+        }
+    }
+    // клас DemoShape успадковує клас DemoPoint і інтерфейс IDemo
+    class DemoShape : DemoPoint, IDemo
+    {
+        protected int z;
+        public DemoShape(int x, int y, int z) : base(x, y)
+        {
+            this.z = z;
+        }
+        // реалізація методу, оголошеного в інтерфейсі, з приховуванням
+       // однойменного методу з базового класу
+    public new void Show()
+        {
+            Console.WriteLine(" точка в пространстве: ({ 0}, { 1}, { 2})", x, y,z);
+        }
+        // реалізація методу, оголошеного в інтерфейсі, з приховуванням
+        //однойменного методу з базового класу
+    public new double Dlina()
+        {
+            return Math.Sqrt(x * x + y * y + z * z);
+        }
+        // реалізація індексатора, оголошеного в інтерфейсі, з приховуванням
+        // однойменного індексатора з базового класу
+        public new int this[int i]
+        {
+            get
+            {
+                if (i == 0) return x;
+                else if (i == 1) return y;
+                else if (i == 2) return z;
+                else throw new Exception(" неприпустиме значення індексу");
+            }
+            set
+            {
+                if (i == 0) x = value;
+                else if (i == 1) y = value;
+
+               
+                else if (i == 2) z = value;
+                else throw new Exception(" неприпустиме значення індексу ");
+            }
+        }
+    }
     class Program
     {
-        const int N = 6;
-        const double pi = 3.14;
-        static void Main(string[] args)
+        static void Main()
         {
-            double[] buferMas = new double[N];
-            using (StreamReader MyFile = new StreamReader("C:\\Users\\USER ONE\\Desktop\\Корзина\\Programming\\Shabolts\\Shabolts\\test.txt"))
+            // створення масиву інтерфейсних посилань
+            IDemo[] a = new IDemo[4];
+            // заповнення масиву
+            a[0] = new DemoPoint(0, 1);
+            a[1] = new DemoPoint(-3, 0);
+            a[2] = new DemoShape(3, 4, 0);
+            a[3] = new DemoShape(0, 5, 6);
+            // перегляд масиву
+            foreach (IDemo x in a)
             {
-               for(int i = 0; i<N;i++)
-               {
-                    buferMas[i] = Convert.ToInt32(MyFile.ReadLine());
-               }
+                x.Show();
+                Console.WriteLine(" Dlina ={ 0:f2}", x.Dlina());
+                Console.WriteLine(" x = " +x.X);
+                x[1] += x[0];
+                Console.Write(" нові координати -");
+                x.Show();
+                Console.WriteLine();
             }
-
-            Figure[] masiv = new Figure[3];
-
-            masiv[0] = new Rectangle();
-            masiv[1] = new Circle();
-            masiv[2] = new Triangle();
-
-            Console.WriteLine("  Прямокутник");
-            masiv[0].Cal_P(buferMas[0], buferMas[1]);
-            masiv[0].Cal_S(buferMas[0], buferMas[1]);
-            masiv[0].ShowInf();
-            Console.WriteLine();Console.WriteLine();
-
-
-            Console.WriteLine("  Коло");
-            masiv[1].Cal_P(buferMas[2], pi);
-            masiv[1].Cal_S(buferMas[2], pi);
-            masiv[1].ShowInf();
-            Console.WriteLine(); Console.WriteLine();
-
-            Console.WriteLine("  Трикутник");
-            masiv[2].Cal_P(buferMas[3], buferMas[4]);
-            masiv[2].Cal_S(buferMas[3], buferMas[5]);
-            masiv[2].ShowInf();
         }
     }
 }
